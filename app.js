87f0705a1292a180e4f50f00e21b332679aca9a4
Daniel Romero <danielpardo6222@gmail.com>
2025-08-12 18:43:44 -0500

    Juego amigo secreto terminado

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let d = document;
+let amigos = [];
+let lista = d.getElementById("listaAmigos");
+let resultado = d.getElementById("resultado");
+
+function agregarAmigo() {
+  if (d.getElementById("amigo").value.trim() !== "") {
+    amigos.push(d.getElementById("amigo").value.trim());
+    limpiarCaja();
+    listaHTML();
+  } else {
+    alert("Por favor, inserte un nombre.");
+  }
+  return;
+}
+
+function limpiarCaja() {
+  return (d.getElementById("amigo").value = "");
+}
+
+function crearLi(texto) {
+  let li = d.createElement("li");
+  li.textContent = texto;
+  return li;
+}
+
+function listaHTML() {
+  lista.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    let li = d.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
+  return;
+}
+
+function sortearAmigo() {
+  if (amigos.length !== 0) {
+    lista.innerHTML = "";
+    resultado.innerHTML = "";
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let nombreSorteado = amigos[indiceAleatorio];
+    let li = d.createElement("li");
+    li.textContent = `El amigo secreto sorteado es: ${nombreSorteado}`;
+    resultado.appendChild(li);
+  } else {
+    alert("No has ingresado nombres para sortear.");
+  }
+  amigos = [];
+}
